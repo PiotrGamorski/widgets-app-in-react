c14d150bc3ced1da1df40743684f66e5676e13f6
Piotr Gamorski <piotr.gamorski@ts.fujitsu.com>
2020-11-26 12:20:01 +0100

    Accordion Widget completed

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -3,21 +3,32 @@ import React, { useState } from "react";
 const Accordion = ({ items }) => {
   const [activeIndex, setActiveIndex] = useState(null);
 
+  //Example to understand the above syntax
+  const colors = ["red", "green"];
+  const redColor = colors[0];
+  const greenColor = colors[1];
+  console.log(redColor, greenColor);
+  //instead we may have a shorter version
+  const [firstElement, secondElement] = colors;
+  console.log(firstElement, secondElement);
+
   const onTitleClick = (index) => {
     setActiveIndex(index);
   };
 
   const renderedItems = items.map((item, index) => {
+    const active = index === activeIndex ? "active" : "";
+
     return (
       <React.Fragment key={item.title}>
         <div
-          className="title active"
+          className={`title ${active}`}
           onClick={() => onTitleClick(index)} //why do we have still arrow function?
         >
           <i className="dropdown icon"></i>
           {item.title}
         </div>
-        <div className="content active">
+        <div className={`content ${active}`}>
           <p>{item.content}</p>
         </div>
       </React.Fragment>
@@ -27,7 +38,6 @@ const Accordion = ({ items }) => {
   return (
     <div className="ui styled accordion">
       {renderedItems}
-      <h1>{activeIndex}</h1>
     </div>
   );
 };
